523e1b651267a76bdb2437fe8bf869803e4fee95
Brian Wardwell <brian.wardwell@codelation.com>
2020-05-07 09:51:08 -0500

    added association between articles and users, updated capybara specs

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -13,6 +13,7 @@ RSpec.feature "Creating Articles" do
         fill_in "Body", with: "Lorem ipsum dolor sit amet, consectetur."
         click_button "Create Article"
         # assertions
+        expect(Article.last.user).to eq(@john)
         expect(page).to have_content("Article has been created")
         expect(page.current_path).to eq(articles_path)
         expect(page).to have_content("Created by: #{@john.email}")
