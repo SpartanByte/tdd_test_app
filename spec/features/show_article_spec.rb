840eb1747339eb18a6ae41394c78e5a1a456cb40
Brian Wardwell <brian.wardwell@codelation.com>
2020-05-07 10:37:56 -0500

    updating article specs

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "Showing an Article" do
     before do
+        john = User.create(email: "john@example.com", password:"password")
+        login_as(john)
         @article = Article.create(title: "The first article",
-        body: "Lorem ipsum dolor sit amet, consectetur.") 
+        body: "Lorem ipsum dolor sit amet, consectetur.", user: john) 
     
         scenario "A user lists all articles" do
             visit "/"
             click_link @article.title
+
             expect(page).to have_content(@article.title) 
             expect(page).to have_content(@article.body) 
             expect(current_path).to eq(article_path(@article))
